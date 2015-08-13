555d9e78a700f1917455d3b907533f0f42e3164f
Curtis Hanson <chanson@mesd.k12.or.us>
2015-08-13 13:11:54 -0700

    add new views, events

---
+++ Form/PermissionType.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * PermissionType.php file
+ *
+ * File that contains the help wiki tag form type form class
+ *
+ * Licence MIT
+ * Copyright (c) 2014 Multnomah Education Service District <http://www.mesd.k12.or.us>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * @filesource /src/Mesd/HelpWikiBundle/Form/PermissionType.php
+ * @package    Mesd\HelpWikiBundle\Form
+ * @copyright  2014 (c) Multnomah Education Service District <http://www.mesd.k12.or.us>
+ * @license    <http://opensource.org/licenses/MIT> MIT
+ * @author     Curtis G Hanson <chanson@mesd.k12.or.us>
+ * @version    0.1.0
+ */
+namespace Mesd\HelpWikiBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+/**
+ * Permission Type
+ *
+ * The help wiki permission form type
+ *
+ * @package    Mesd\HelpWikiBundle\Form
+ * @copyright  2015 (c) Multnomah Education Service District <http://www.mesd.k12.or.us>
+ * @license    <http://opensource.org/licenses/MIT> MIT
+ * @author     Curtis G Hanson <chanson@mesd.k12.or.us>
+ * @since      0.2.0
+ */
+class PermissionType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('object', 'mesd_help_wiki_object_type')
+            ->add('type', 'mesd_help_wiki_permission_type')
+            ->add('role')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Mesd\HelpWikiBundle\Entity\Permission'
+        ));
+    }
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'mesd_helpwikibundle_permission';
+    }
+}
